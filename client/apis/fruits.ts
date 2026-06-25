6be0bd2926bf7b1b9eba8a099308167b35e10de4
daniel-fallows <daniel.dev.nz@gmail.com>
2026-06-25 12:13:00 +1200

    remove boilerplate files for T01

--- client/apis/fruits.ts
+++
@@ -1,8 +0,0 @@
-import request from 'superagent'
-
-const rootURL = new URL(`/api/v1`, document.baseURI)
-
-export async function getFruits(): Promise<string[]> {
-  const response = await request.get(`${rootURL}/fruits`)
-  return response.body.fruits as string[]
-}
