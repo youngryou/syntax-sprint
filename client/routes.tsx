67b62a5e11d1d847b2a40de9c5e66f80a60e2381
Young Ryou <ryouyt@gmail.com>
2026-06-25 17:14:36 +1200

    feat: add nav links to App component and styles

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,12 +1,12 @@
 import { createRoutesFromElements, Route } from 'react-router'
-import Layout from './components/Layout.tsx'
+import App from './components/App.tsx'
 import Dashboard from './pages/Dashboard.tsx'
 import Auth from './pages/Auth.tsx'
 import Arena from './pages/Arena.tsx'
 import Profile from './pages/Profile.tsx'
 
 export default createRoutesFromElements(
-  <Route path="/" element={<Layout />}>
+  <Route path="/" element={<App />}>
     <Route index element={<Dashboard />} />
     <Route path="auth" element={<Auth />} />
     <Route path="arena" element={<Arena />} />
