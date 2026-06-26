ea2f9790db85a15714f1f78a555ed1eea4a65882
Maneet <maneetsinghbajaj6@gmail.com>
2026-06-26 18:02:30 +1200

    working on scores.ts routing

---
+++ server/Routes/scores.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import * as db from '../../server/db/supabase'
+
+const router = express.Router()
+export default router
+
+router.post('/', requireAuth, (req,res) => {
+  try {
+    const 
+  }
+})
