85e8027a5c22e50a25e5cf6bab00bd31dac6db93
Parkie Park <parkie0621@gmail.com>
2026-06-29 16:13:24 +1200

    changes

--- server/db/users.ts
+++ server/db/users.ts
@@ -1,17 +1,40 @@
 import { supabase } from './supabase'
-import { User } from '../../models/user'
+import { User, NewUser } from '../../models/user'
+
+const userData =
+  'userId:id, username, profileImage:profile_image, joinedAt:created_at'
 
 export async function getUserById(userId: string): Promise<User | null> {
   const { data, error } = await supabase
     .from('users')
-    .select('userId:user_id, username, profileImage:profile_image, joinedAt:joined_at')
-    .eq('user_id', userId)
+    .select(userData)
+    .eq('id', userId)
     .single()
 
   if (error) {
-    if (error.code === 'PGRST116') return null
-    throw new Error(`Supabase DB Error: ${error.message}`)
+    if (error.code === 'PGRST116') {
+      return null
+    }
+    throw new Error(`Supabase DB Error (getUserById): ${error.message}`)
   }
+  return data as User
+}
 
+export async function addUser(newUser: NewUser): Promise<User> {
+  const { data, error } = await supabase
+    .from('users')
+    .insert([
+      {
+        id: newUser.id,
+        username: newUser.username,
+        profile_image: newUser.profileImage,
+      },
+    ])
+    .select(userData)
+    .single()
+
+  if (error) {
+    throw new Error(`supabase DB Error (addUser): ${error.message}`)
+  }
   return data as User
 }
