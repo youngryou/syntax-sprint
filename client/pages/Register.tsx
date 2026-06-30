b40cc9be1302370150a4aa29b98100b7a596d0d4
Young Ryou <ryouyt@gmail.com>
2026-06-30 15:40:35 +1200

    feat: add initial register page and route

---
+++ client/pages/Register.tsx
@@ -0,0 +1,14 @@
+export default function Register() {
+  return (
+    <div className="layout-container layout--half">
+      <div className="card">
+        <div className="text--center">
+          <h2 className="page-title">Welcome Back</h2>
+          <p className="card-subtitle text-muted">
+            Sign in to save your typing scores.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
